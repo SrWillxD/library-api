5b81a49c495a1036cd20894c0b02c8849fe02c82
SrWillxD(araujo) <willgra09@hotmail.com>
2023-09-21 10:48:24 -0300

    Creating model for clients table

---
+++ src/app/Models/clients.model.js
@@ -0,0 +1,32 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../Database/ElephantSQL/database.js';
+
+const Client = sequelize.define('Client', {
+    client_id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name:{
+        type: DataTypes.STRING(255),
+        allowNull: false,
+    },
+    email:{
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+    },
+    tell: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+    },
+    address: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+    },
+},{tableName: 'clients', timestamps: false}
+);
