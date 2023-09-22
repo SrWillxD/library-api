3340645d66a4b630d58f968fa9361d498e6ccda8
SrWillxD <willgra09@hotmail.com>
2023-09-22 18:12:57 -0300

    Books models created

---
+++ src/app/Models/books.model.js
@@ -0,0 +1,34 @@
+// models/Book.js
+import { DataTypes } from 'sequelize';
+import sequelize from '../Database/ElephantSQL/database.js';
+import Author from './authors.model.js';
+
+const Book = sequelize.define('Book', {
+    book_id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    title: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+    },
+    price: {
+        type: DataTypes.NUMERIC(10, 2),
+        allowNull: false,
+    },
+    stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    author_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Author,
+            key: 'author_id',
+        },
+    },
+}, { tableName: 'books', timestamps: false });
+
+export default Book;
