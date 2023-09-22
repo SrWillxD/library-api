db153b90ce3a12213ff1d95f7fe8311d653c84b3
SrWillxD <willgra09@hotmail.com>
2023-09-21 21:52:30 -0300

    Method to update a client, created

--- src/app/Controllers/clients.controller.js
+++ src/app/Controllers/clients.controller.js
@@ -20,9 +20,39 @@ const clientsControllerOBJ = {
             return res.status(201).json({message: "Success", client: newClient});
         } catch (err){
             console.error("Error when registering a client", err);
-            return res.status(500).json({message: " Internal Server error"});
+            return res.status(500).json({message: "Internal Server error"});
         }
-    }
+    },
+    async updateClient(req, res, next){
+        try {
+            const { id } = req.params;
+            const { name, email, password, tell, address } = req.body;
+    
+            if (!name || !email || !password || !tell || !address) {
+                return res.status(400).json({ message: 'Todos os campos são obrigatórios.' });
+            }
+    
+            const existingClient = await Client.findByPk(id);
+    
+            if (!existingClient) {
+                return res.status(404).json({ message: 'Client ID not found.' });
+            }
+    
+            existingClient.name = name;
+            existingClient.email = email;
+            existingClient.password = password;
+            existingClient.tell = tell;
+            existingClient.address = address;
+    
+            await existingClient.save();
+    
+            return res.status(200).json({message: "Success", client: existingClient});
+        } catch (err) {
+            console.error('Error updating client:', err);
+            return res.status(500).json({ message: 'Internal Server error.' });
+        }
+    },
+
 }
 
 export default clientsControllerOBJ;
--- src/app/Routes/clients.route.js
+++ src/app/Routes/clients.route.js
@@ -3,5 +3,6 @@ const routes = express.Router();
 import clientsControllerOBJ from '../Controllers/clients.controller.js'
 
 routes.post('/registerclient', clientsControllerOBJ.registerClient);
+routes.put('/updateclient/:id', clientsControllerOBJ.updateClient);
 
 export default routes;
