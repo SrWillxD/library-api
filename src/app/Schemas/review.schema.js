8803eb3c321c8321c45440eed5e07f24df97bf46
SrWillxD(araujo) <willgra09@hotmail.com>
2023-09-27 09:49:00 -0300

    Review schema created

---
+++ src/app/Schemas/review.schema.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+
+const reviewSchema = new mongoose.Schema({
+    name: String,
+    grade: Number,
+    review: String,
+});
+
+const bookSchema = new mongoose.Schema({
+    bookId: Number,
+    description: String,
+    pages: Number,
+    publisher: String,
+    reviews: [reviewSchema],
+});
+
+const InfoBooksReview = mongoose.model('Book', bookSchema);
+
+export default InfoBooksReview;
